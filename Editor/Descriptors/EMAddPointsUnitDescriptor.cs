7f3fbd25bd11a45aa62983da23d2a09889a4ccb2
Jason Millard <jsm174@gmail.com>
2022-06-18 07:44:12 -0400

    em: Start to implement EMAddPointsUnit

---
+++ Editor/Descriptors/EMAddPointsUnitDescriptor.cs
@@ -0,0 +1,45 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2022 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// ReSharper disable UnusedType.Global
+
+using Unity.VisualScripting;
+
+namespace VisualPinball.Unity.VisualScripting.Editor
+{
+	[Descriptor(typeof(EMAddPointsUnit))]
+	public class EMAddPointsUnitDescriptor : UnitDescriptor<EMAddPointsUnit>
+	{
+		public EMAddPointsUnitDescriptor(EMAddPointsUnit target) : base(target)
+		{
+		}
+
+		protected override string DefinedSummary()
+		{
+			return "This node adds points for EMs.";
+		}
+
+		protected override EditorTexture DefinedIcon() => EditorTexture.Single(Unity.Editor.Icons.PlayerVariable);
+
+		protected override void DefinedPort(IUnitPort port, UnitPortDescription desc)
+		{
+			base.DefinedPort(port, desc);
+
+			switch (port.key) {
+			}
+		}
+	}
+}
